557efcf020c6abbe07f04e0f65cb8e858dd939ab
Adil Naimi <adilnm23@gmail.com>
2020-07-09 17:26:50 -0500

    Save user to the database

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,34 @@
 const express = require("express");
-const router=express.Router()
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
-router.get('/',(req,res)=>{
-    res.send("hello")
-})
+router.get("/", (req, res) => {
+  res.send("hello");
+});
 
-router.post('/signup',(req,res)=>{
-    const{name,email,password}=req.body
-    if (!email||!password||!name) {
-        return res.status(422).send({error:"please add all the fields"})
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).send({ error: "please add all the fields" });
+  }
+  User.findOne({ email: email })
+    .then(savedUser => {
+      if (savedUser) {
+        return res.status(422).send({ error: "user already exists" });
+      }
+      const user = new User({
+        email,
+        name,
+        password
+      });
+      user.save()
+        .then(user => {
+          res.json({ message: "saved successfully" });
+        })
+        .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
+});
 
-    }
-    res.send({message:"Success"})
-})
-
-module.exports=router
+module.exports = router;
