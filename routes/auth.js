fe47429e9fb4c671282539df51e39f7b9de19ad9
Adil Naimi <adilnm23@gmail.com>
2020-07-09 15:45:28 -0500

    Test signup

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,13 @@ router.get('/',(req,res)=>{
     res.send("hello")
 })
 
+router.post('/signup',(req,res)=>{
+    const{name,email,password}=req.body
+    if (!email||!password||!name) {
+        return res.status(422).send({error:"please add all the fields"})
+
+    }
+    res.send({message:"Success"})
+})
+
 module.exports=router
