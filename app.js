5b1f01383b40edf1ef86e779717243812ddb9ec4
Adil Naimi <adilnm23@gmail.com>
2020-07-08 23:24:31 -0500

    Create a model

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const mongoose = require("mongoose");
 const PORT = 5000;
 const { MONGOURI } = require("./keys");
 
+require("./models/user");
+
+
+app.use(require('./routes/auth'))
+
 mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on("connected", () => {
   console.log("connected to mongo");
 });
+
 mongoose.connection.on("error", err => {
   console.log("error connecting", err);
 });
+
 app.listen(PORT, () => {
   console.log("server runnin", PORT);
 });
