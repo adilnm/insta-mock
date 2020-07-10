361958e65d946206df6c2da73244ae2d23ca07a5
Adil Naimi <adilnm23@gmail.com>
2020-07-10 04:18:54 -0500

    Show all posts

--- routes/post.js
+++ routes/post.js
@@ -2,22 +2,34 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
-const Post=mongoose.model("Post")
+const Post = mongoose.model("Post");
 
 router.post("/createpost", requireLogin, (req, res) => {
   const { title, body } = req.body;
   if (!title || !body) {
     return res.status(422).json({ error: "please add all the fields" });
   }
-  req.user.password=undefined //remove the user password from the returned post 
+  req.user.password = undefined; //remove the user password from the returned post
   const post = new Post({
     title,
     body,
     postedBy: req.user
   });
-  post.save()
+  post
+    .save()
     .then(result => {
       res.json({ post: result });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.get("/allposts", (req, res) => {
+  Post.find()
+    .populate("postedBy","_id name") //Show the user info only the name & id
+    .then(posts => {
+      res.json({ posts });
     })
     .catch(error => {
       console.log(error);
