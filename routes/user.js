6db1926f68ae02e0342c147a8d810b3c9e9eb094
Adil Naimi <adilnm23@gmail.com>
2020-07-24 13:18:21 -0500

    Setup follow and unfollow routes

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const requireLogin = require("../middleware/requireLogin");
 const Post = mongoose.model("Post");
 const User = mongoose.model("User");
 
-router.get("/user/:id", (req, res) => {
+router.get("/user/:id", requireLogin, (req, res) => {
   User.findOne({ _id: req.params.id })
     .select("-password")
     .then(user => {
@@ -21,5 +21,61 @@ router.get("/user/:id", (req, res) => {
     .catch(err => {
       return res.status(404).json({ error: "User not found" });
     });
+});
+
+router.put("/follow", requireLogin, (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.followId,
+    {
+      $push: { followers: req.user._id }
+    },
+    { new: true },
+    (err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      }
+      User.findByIdAndUpdate(
+        req.user._id,
+        {
+          $push: { following: req.body.followId }
+        },
+        { new: true }
+      )
+        .then(result => {
+          res.json(result);
+        })
+        .catch(err => {
+          return res.status(422).json({ error: err });
+        });
+    }
+  );
+});
+
+router.put("/unfollow", requireLogin, (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.unfollowId,
+    {
+      $pull: { followers: req.user._id }
+    },
+    { new: true },
+    (err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      }
+      User.findByIdAndUpdate(
+        req.user._id,
+        {
+          $push: { following: req.body.unfollowId }
+        },
+        { new: true }
+      )
+        .then(result => {
+          res.json(result);
+        })
+        .catch(err => {
+          return res.status(422).json({ error: err });
+        });
+    }
+  );
 });
 module.exports = router;
