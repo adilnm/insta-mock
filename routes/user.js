e8d3cae0cd1732cf001cfd2ac8e7913443731d83
Adil Naimi <adilnm23@gmail.com>
2020-07-26 02:02:11 -0500

    Create url for pic upload

--- routes/user.js
+++ routes/user.js
@@ -43,12 +43,11 @@ router.put("/follow", requireLogin, (req, res) => {
       )
         .select("-password")
         .then(user => {
-          Post.find({ postedBy: req.body.followId })
-          .exec((err, posts) => {
+          Post.find({ postedBy: req.body.followId }).exec((err, posts) => {
             if (err) {
               return res.status(402).json({ error: err });
             }
-            res.json({ user:result, posts });
+            res.json({ user: result, posts });
           });
         })
         .catch(err => {
@@ -76,19 +75,32 @@ router.put("/unfollow", requireLogin, (req, res) => {
         },
         { new: true }
       )
-      .select("-password")
-      .then(user => {
-        Post.find({ postedBy: req.body.followId })
-        .exec((err, posts) => {
-          if (err) {
-            return res.status(402).json({ error: err });
-          }
-          res.json({ user:result, posts });
-        });
-      })
+        .select("-password")
+        .then(user => {
+          Post.find({ postedBy: req.body.followId }).exec((err, posts) => {
+            if (err) {
+              return res.status(402).json({ error: err });
+            }
+            res.json({ user: result, posts });
+          });
+        })
         .catch(err => {
           return res.status(422).json({ error: err });
         });
+    }
+  );
+});
+
+router.put("/updatepic", requireLogin, (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    { $set: { pic: req.body.pic } },
+    { new: true },
+    (err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      }
+      res.json(result);
     }
   );
 });
